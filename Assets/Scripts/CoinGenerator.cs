7aa3346ce28d3b2b254a1ae4ca24cf57b81a2d32
Anvar <Anvar-abd_97@mail.ru>
2018-09-13 17:34:34 +0500

    Coin Generator
    Fixed:
    -.......

--- Assets/Scripts/CoinGenerator.cs
+++ Assets/Scripts/CoinGenerator.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CoinGenerator : MonoBehaviour {
 	public ObjectPooler coinPool;
 	public float distanceBetweenCoins;
+
     private GameObject coin;
     private int numberOfCoins;
     private int numberOfBlocks;
-    private int startingPoint;
+    private int firstStartingPoint;
+
 	public void SpawnCoins(int platformSelector, float platformWidth, Vector3 startPosition)
 	{
         switch (platformSelector) {
@@ -25,11 +27,22 @@ public class CoinGenerator : MonoBehaviour {
                 numberOfBlocks = 9;
                 break;
         }
-        
-        coin = coinPool.GetPooledObject();
-        coin.transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2), startPosition.y, startPosition.z);
 
-        coin.SetActive(true);
+        // Getting Random position on the platform
+        firstStartingPoint = Random.Range(0, numberOfBlocks);
+
+        // Getting Random number of coins
+        numberOfCoins = Random.Range(1, numberOfBlocks + 1);
+
+        for (int i = 0; i < numberOfCoins; i++) {
+            coin = coinPool.GetPooledObject();
+            coin.transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2) + firstStartingPoint + 1, startPosition.y, startPosition.z);
+            coin.SetActive(true);
+        }
+       // coin = coinPool.GetPooledObject();
+       // coin.transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2), startPosition.y, startPosition.z);
+        
+        
 
 		/*GameObject coin1 = coinPool.GetPooledObject();
 		coin1.transform.position = startPosition;
