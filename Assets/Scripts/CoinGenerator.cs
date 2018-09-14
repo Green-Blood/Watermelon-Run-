1edf7cb67f6806712262f1cbf671e515700ec5ad
Anvar <Anvar-abd_97@mail.ru>
2018-09-14 14:10:11 +0500

    Coins Fix
    Fixed:
    -Coins Generate randomly in different positions

--- Assets/Scripts/CoinGenerator.cs
+++ Assets/Scripts/CoinGenerator.cs
@@ -6,7 +6,7 @@ public class CoinGenerator : MonoBehaviour {
 	public ObjectPooler coinPool;
 	public float distanceBetweenCoins;
 
-    private GameObject coin;
+    private GameObject[] coin;
     private int numberOfCoins;
     private int numberOfBlocks;
     private int firstStartingPoint;
@@ -30,14 +30,14 @@ public class CoinGenerator : MonoBehaviour {
 
         // Getting Random position on the platform
         firstStartingPoint = Random.Range(0, numberOfBlocks);
-
+        coin = new GameObject[numberOfBlocks];
         // Getting Random number of coins
         numberOfCoins = Random.Range(1, numberOfBlocks + 1);
 
         for (int i = 0; i < numberOfCoins; i++) {
-            coin = coinPool.GetPooledObject();
-            coin.transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2) + firstStartingPoint + 1, startPosition.y, startPosition.z);
-            coin.SetActive(true);
+            coin[i] = coinPool.GetPooledObject();
+            coin[i].transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2) + firstStartingPoint + i, startPosition.y, startPosition.z);
+            coin[i].SetActive(true);
         }
        // coin = coinPool.GetPooledObject();
        // coin.transform.position = new Vector3(startPosition.x - (platformWidth / 2) + (distanceBetweenCoins / 2), startPosition.y, startPosition.z);
