9074dc212516714fc5338a3558ad31f241076f82
Anvar <Anvar-abd_97@mail.ru>
2019-01-12 00:30:37 +0500

    do not know

--- Assets/ScrollingBackground.cs
+++ Assets/ScrollingBackground.cs
@@ -13,21 +13,65 @@ public class ScrollingBackground : MonoBehaviour
 {
 
   #region Variables
-    private Transform cameraTransform;
 
-    private Transform[] layers;
-    private float viewZone = 10;
-    private float leftIndex;
+  public float backgroundSize;
+
+  private Transform cameraTransform;
+  private Transform[] layers;
+  private float viewZone = 10;
+  private int leftIndex;
+  private int rightIndex;
+
 
   #endregion
 
   void Start()
   {
-
+    cameraTransform = FindObjectOfType<CameraController>().transform;
+    layers = new Transform[transform.childCount];
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      layers[i] = transform.GetChild(i);
+    }
+    leftIndex = 0;
+    rightIndex = layers.Length - 1;
   }
 
   void Update()
   {
+    // if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+    // {
+    //   ScrollLeft();
+    // }
+    if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+    {
+      ScrollRight();
+    }
 
+  }
+
+
+  private void ScrollLeft()
+  {
+    int lastRight = rightIndex;
+    layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+    leftIndex = rightIndex;
+    rightIndex--;
+    if (rightIndex < 0)
+    {
+      rightIndex = layers.Length - 1;
+    }
+  }
+
+  private void ScrollRight()
+  {
+    int lastLeft = leftIndex;
+    layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+    rightIndex = leftIndex;
+    leftIndex++;
+    if (leftIndex == layers.Length)
+    {
+      leftIndex = 0;
+    }
   }
 }
